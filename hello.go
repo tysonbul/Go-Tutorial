acf908b2eeb13ac53f85d567e9dcfc205e9c6cc8
tysonbul <tyson_bulmer@hotmail.com>
2017-10-13 20:40:39 -0700

    Add import of stringutil and Reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+
+	"fmt"
+	
+	"github.com/tysonbul/stringutil"
+)
 
 func main() {
-    fmt.Printf("Hello World!\n")
+    fmt.Printf(stringutil.Reverse("!oG, olleH"))
 }
